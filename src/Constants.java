5f548796e2b19199ea44a8865cedc481539bac7c
Primeeight <zacharywray@gmail.com>
2023-09-30 14:13:12 -0400

    9/30/23

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,6 @@
 public class Constants {
 	public static final int buffersize = 13;
 	public static final int lostframes = 0;
-	public static final int timeout = 1000;
+	public static final int timeout = 10;
 }
 
